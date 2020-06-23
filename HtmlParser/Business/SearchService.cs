5f96edc1c492d7302f99a23340fbd220670984e2
Julius <yulii.vedkal@nure.ua>
2020-06-23 20:10:52 +0300

    [update] - refactore SearchService + add descriptions

--- HtmlParser/Business/SearchService.cs
+++ HtmlParser/Business/SearchService.cs
@@ -1,4 +1,5 @@
-﻿using AngleSharp.Dom;
+﻿using AngleSharp;
+using AngleSharp.Dom;
 using HtmlParser.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,31 @@ namespace HtmlParser.Business
 {
     public static class SearchService
     {
+        #region Public Methods
+
+        /// <summary>
+        /// Search html subtrees by source html code and template. Returns a collection of nodes containing html nodes with desired characteristics.
+        /// </summary>
+        /// <typeparam name="TModel">Parsing Model. Must be a class with a default constructor.</typeparam>
+        /// <param name="html">The source html code.</param>
+        /// <param name="node">The parsing template.</param>
+        /// <returns></returns>
+        public static IEnumerable<INode<TModel>> SearchBlock<TModel>(string html, INode<TModel> node) where TModel : class, new()
+        {
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var document = context.OpenAsync(req => req.Content(html)).GetAwaiter().GetResult();
+
+            return SearchBlock(document, node);
+        }
+
+        /// <summary>
+        /// Search html subtrees by anglesharp source document and template. Returns a collection of nodes containing html nodes with desired characteristics.
+        /// </summary>
+        /// <typeparam name="TModel">Parsing Model. Must be a class with a default constructor.</typeparam>
+        /// <param name="document">The AngelSharp resource document.</param>
+        /// <param name="node">The parsing template.</param>
+        /// <returns></returns>
         public static IEnumerable<INode<TModel>> SearchBlock<TModel>(INode document, INode<TModel> node) where TModel : class, new()
         {
             var result = new List<INode<TModel>>();
@@ -31,6 +57,13 @@ namespace HtmlParser.Business
             return result;
         }
 
+        /// <summary>
+        /// Search html subtrees by anglesharp source document and collection of template. Returns a collection of nodes containing html nodes with desired characteristics.
+        /// </summary>
+        /// <typeparam name="TModel">Parsing Model. Must be a class with a default constructor.</typeparam>
+        /// <param name="document">The AngelSharp resource document.</param>
+        /// <param name="nodes">The collection of parsing templates</param>
+        /// <returns></returns>
         public static IEnumerable<INode<TModel>> SearchBlock<TModel>(INode document, IEnumerable<INode<TModel>> nodes) where TModel : class, new()
         {
             var result = new List<INode<TModel>>();
@@ -56,60 +89,81 @@ namespace HtmlParser.Business
             return result;
         }
 
-        public static INode<TModel> ContainsThisBlocks<TModel>(IElement element, INode<TModel> node)
+        /// <summary>
+        /// Search the subtree in the anglesharp element by node template. Returns a node with desired characteristics.
+        /// </summary>
+        /// <typeparam name="TModel">Parsing Model. Must be a class with a default constructor</typeparam>
+        /// <param name="element">The AngleSharp resource element.</param>
+        /// <param name="node">The template node.</param>
+        /// <returns></returns>
+        public static INode<TModel> ContainsThisBlocks<TModel>(IElement element, INode<TModel> node) where TModel : class, new()
         {
-            if (element?.TagName?.ToLower() == node.Name && 
-                (!node.Attributes.Any() || node.Attributes.All(pair => element.Attributes.Any(attr => attr.Name == pair.Key && 
-                                                                                                     attr.Value == pair.Value))) &&
-                (string.IsNullOrWhiteSpace(node.InnerText) || element.TextContent.Contains(node.InnerText)))
+            var result = default(INode<TModel>);
+
+            if (VerifyElement(element, node))
             {
                 if(!node.SubNodes.Any())
                 {
                     node.Element = element;
-                    return node;
+                    result = node;
                 }
-                var pairs = new List<KeyValuePair<IElement, INode<TModel>>>();
-                var children = new List<IElement>(element.Children);
-                foreach (var subNode in node.SubNodes)
-                {
-                    var elem = children.FirstOrDefault(el => el.TagName.ToLower() == subNode.Name && 
-                                                             (!subNode.Attributes.Any() || subNode.Attributes.All(pair => el.Attributes.Any(attr => attr.Name.ToLower() == pair.Key &&
-                                                                                                                          attr.Value.ToLower() == pair.Value))) &&
-                                                             (string.IsNullOrWhiteSpace(subNode.InnerText) || el.TextContent.Contains(subNode.InnerText)));
-                    if (elem != null)
+                else 
+                { 
+                    var pairs = new List<KeyValuePair<IElement, INode<TModel>>>();
+                    var children = new List<IElement>(element.Children);
+
+                    foreach (var subNode in node.SubNodes)
                     {
-                        pairs.Add(new KeyValuePair<IElement, INode<TModel>>(elem, subNode));
-                        children.Remove(elem);
-                    }
-                }
-                if(pairs.Count == node.SubNodes.Count)
-                {
-                    var result = new List<INode<TModel>>();
-                    for (int i = 0; i < pairs.Count && result.Count == i; i++)
-                    {
-                        var res = ContainsThisBlocks(pairs[i].Key, pairs[i].Value);
-                        if(res != null)
+                        var elem = children.FirstOrDefault(el => VerifyElement(el, subNode));
+                        if (elem != null)
                         {
-                            result.Add((INode<TModel>)res.Clone());
+                            pairs.Add(new KeyValuePair<IElement, INode<TModel>>(elem, subNode));
+                            children.Remove(elem);
                         }
                     }
-                    
-                    if (result.Count == pairs.Count)
-                    {
-                        node.Element = element;
-                        node.SubNodes = result;
-                        node = (INode<TModel>)node.Clone();
-                    }
-                    else
-                    {
-                        node = null;
-                    }
 
-                    return node;
+                    if (pairs.Count == node.SubNodes.Count)
+                    {
+                        var subnodes = new List<INode<TModel>>();
+                        for (int i = 0; i < pairs.Count && subnodes.Count == i; i++)
+                        {
+                            INode<TModel> res = ContainsThisBlocks(pairs[i].Key, pairs[i].Value);
+                            if (res != null)
+                            {
+                                subnodes.Add((INode<TModel>)res.Clone());
+                            }
+                        }
+
+                        if (subnodes.Count == pairs.Count)
+                        {
+                            node.Element = element;
+                            node.SubNodes = subnodes;
+                            node = (INode<TModel>)node.Clone();
+                        }
+                        else
+                        {
+                            node = null;
+                        }
+
+                        result = node;
+                    }
                 }
             }
 
-            return null;
+            return result;
         }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private static bool VerifyElement<TModel>(IElement element, INode<TModel> node) where TModel : class, new() =>
+            element?.TagName?.ToLower() == node.Name &&
+            (!node.Attributes.Any() || node.Attributes.All(pair => element.Attributes.Any(attr => attr.Name == pair.Key &&
+                                                                                                  attr.Value == pair.Value))) &&
+            (string.IsNullOrWhiteSpace(node.InnerText) || element.TextContent.Contains(node.InnerText));
+
+        #endregion Private Methods
+
     }
 }
